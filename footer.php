735e48ae0fd936cfda51569bd591410ca89a36e3
olivier de Méringo <demeringo@gmail.com>
2011-02-04 19:37:04 +0100

    Credits

--- footer.php
+++ footer.php
@@ -19,21 +19,25 @@
 			<a href="<?php bloginfo('rss2_url'); ?>">Flux RSS des articles</a>
 			</p>
 			<p>
-			Contact: <a href="mailto:webmaster@samba-resille.org">webmaster@samba-resille.org</a>
+			<?php wp_loginout( get_permalink(), true ); ?>  
 			</p>
 			
 			<p>
-			Samba Résille est une association de loi 1901.
+			<strong>Samba Résille est une association de loi 1901.</strong>
 			<br/>
 			Agréée Jeunesse et Education Populaire n° 31-580 / Agréée Entreprise Solidaire n°31-012<br/>
 			Siret n° : 403 391 857 00030 / Code APE: 9001Z<br/>
 			Entrepreneur du Spectacle: Licence 1-1012395 / Licence 2-1012396 / Licence 3-1012397
 			<p>
-			Site sous licence Creative Commons.Toute reproduction interdite sans autorisation préalable.
+			<strong>Le site:</strong><br/>
+			Contact: <a href="mailto:webmaster@samba-resille.org">webmaster@samba-resille.org</a><br/>
+			Consultation en direction artistique et en ergonomie : <a href="mailto:charles.briens@wanadoo.fr">Charles Briens</a><br/>
+			Réalisation: <a href="mailto:olivier@le-moulin-de-verre.com">Olivier de Méringo</a> et Samba Résille.
 			</p>
 			<p>
-			<?php wp_loginout( get_permalink(), true ); ?>  
+			Toute reproduction interdite sans autorisation préalable.
 			</p>
+			
 
 			<!-- #site-generator -->
 
