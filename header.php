50f65d5231938eaabdf58429b556378c75071052
olivier de Méringo <demeringo@gmail.com>
2012-09-18 00:53:58 +0200

    Back to standard header.

--- header.php
+++ header.php
@@ -82,10 +82,29 @@
 					</span>
 				</<?php echo $heading_tag; ?>>
 				<div id="site-description"><?php bloginfo( 'description' ); ?></div>
-				<img src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" />
-				<!--
-				<img src="<?php echo child_template_directory ?>/img/banniere-940x87.jpg" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" />
-				-->
+
+				<?php
+					// Check if this is a post or page, if it has a thumbnail, and if it's a big one
+					if ( is_singular() &&
+							has_post_thumbnail( $post->ID ) &&
+							( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail' ) ) &&
+							$image[1] >= HEADER_IMAGE_WIDTH ) :
+						// Houston, we have a new header image!
+						//echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );
+						//Use standard banner instead of custom banner depending on post
+						?>
+						<img src="<?php echo child_template_directory ?>/img/banniere-940x87.jpg" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" />
+						<?php
+					else : ?>
+						<!-- 
+						default configurable header image (commented out)
+						-->
+						<!--
+						<img src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" />-->
+						<img src="<?php echo child_template_directory ?>/img/banniere-940x87.jpg" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" />
+
+					<?php endif; ?>
+			
 			</div><!-- #branding -->
 			
 			<div id="access" role="navigation">
@@ -96,9 +115,10 @@
 				
 
 			</div><!-- #access -->
+			
 			<div id="short-menu" class="alpha60">
-					<a href="/site/contacts/" >Contact - Accès</a><br/>
-					<a href="/site/espace-presse/">Espace presse</a><br/>
+					<a href="http://samba-resille.org/" >Accéder au site public</a><br/>
+					<!--<a href="/site/espace-presse/">Espace presse</a><br/>-->
 				</div>
 		</div><!-- #masthead -->
 			</div><!-- #header -->
