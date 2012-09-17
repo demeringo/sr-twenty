8ddd2511d3d80ac7669633768d778330284f598c
olivier de Méringo <demeringo@gmail.com>
2012-09-18 00:53:14 +0200

    Re activate the widget area on right.

--- sidebar.php
+++ sidebar.php
@@ -8,9 +8,16 @@
  */
 ?>
 
-	    <div id="primary" class="widget-area" role="complementary">
+        <div id="primary" class="widget-area" role="complementary">
+            <ul class="xoxo">
+<?php
+    /* When we call the dynamic_sidebar() function, it'll spit out
+     * the widgets for that widget area. If it instead returns false,
+      * then the sidebar simply doesn't exist, so we'll hard-code in
+     * some default sidebar stuff just in case.
+      */
+      if ( ! dynamic_sidebar( 'primary-widget-area' ) ) : ?> <div id="primary" class="widget-area" role="complementary">
 			<div id="event-sidebar">
-			
 				<h1>Programmation</h1>
 				<?php $my_query = new WP_Query('category_name=evenements&posts_per_page=3'); ?>
 					<?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
@@ -22,4 +29,11 @@
 				<?php endwhile; ?>
 				<p><a href="/site/category/evenements/">Toute la programmation →</a></p>
 			</div>
-		</div><!-- #primary .widget-area -->
+		</div>
+<?php endif; // end primary widget area ?>
+            </ul>
+        </div><!-- #primary .widget-area -->
+
+
+
+	    
